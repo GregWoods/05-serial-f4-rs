52c2c32ba02681b5de0f58909d5ecb75a846721c
Greg Woods <gregwoodslancs@gmail.com>
2020-05-18 18:53:31 +0100

    shortened initial git commit message

--- git-setup.bat
+++ git-setup.bat
@@ -2,5 +2,5 @@ Rem run after cargo generate [url] --name [new-project-name]
 gh repo create --public {{project-name}}
 git init
 git add -A
-git commit -m "Created using cargo generate --git https://github.com/gregwoods/stm32f4-05-serial --name {{project-name}}"
+git commit -m "Created using cargo generate from /gregwoods/stm32f4-05-serial"
 git push -u origin master
